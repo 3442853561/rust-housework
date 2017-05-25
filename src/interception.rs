dac08f8e8d6cda47ce3cf56db35920080a85b818
刘祺 <3442853561@qq.com>
2017-05-25 17:50:50 +0800

    Update interception.rs

--- src/interception.rs
+++ src/interception.rs
@@ -1,52 +1,91 @@
+use std::convert::AsMut;
 trait Trait {
-	fn cut(&mut self, long: usize) -> Self;
-	fn take(&mut self, start: usize, end: usize) -> Self;
-	fn flashback(&mut self) -> Self;
+	fn take(&mut self, start: usize, end: usize);
 }
 
 impl<T> Trait for Vec<T> {
-	fn cut(&mut self, long: usize) -> Self {
-		let mut foo: Self = Vec::new();
-		for _ in 0..long {
-			foo.push(self.pop().unwrap());
-		}
-		foo
-	}
-	fn take(&mut self, start: usize, end: usize) -> Self {
+	fn take(&mut self, start: usize, end: usize){
 		let cut = self.len() - end - 1;
-		self.cut(cut);
+		for _ in 0..cut {
+			self.pop();
+		}
 		let mut foo: Self = Vec::new();
 		for _ in start..end + 1 {
 			foo.push(self.pop().unwrap());
 		}
-		foo
-	}
-	fn flashback(&mut self) -> Self {
-		let mut foo: Self = Vec::new();
-		for _ in 0..self.len() {
-			foo.push(self.pop().unwrap());
+		*self = Vec::new();
+		for _ in 0..foo.len() {
+			self.push(foo.pop().unwrap());
 		}
-		foo
 	}
 }
 
-fn main() {
-	let mut foo: Vec<u8> = vec![0, 2, 1, 0, 3, 1];
+fn avaricious<T>(foo: &mut Vec<T>, start_token: T, end_token: T)
+where T: PartialEq {
+	let mut start = 0;
+	loop {
+		if foo[start] == start_token {
+			break;
+		}
+		start += 1;
+	}
 	let mut end = foo.len() - 1;
 	loop {
-		if foo[end] == 1 {
+		if foo[end] == end_token {
 			break;
 		}
 		end -= 1;
 	}
-	let mut start = end;
+
+	foo.take(start, end);
+}
+
+fn header<T>(foo: &mut Vec<T>, start_token: T, end_token: T)
+where T: PartialEq {
+	let mut start = 0;
 	loop {
-		start -= 1;
-		if foo[start] == 0 {
+		if foo[start] == start_token {
 			break;
 		}
-		
+		start += 1;
 	}
-	println!("{:?}", foo.take(start, end).flashback());
-	
+	let mut end = start + 1;
+	loop {
+		if foo[end] == end_token {
+			break;
+		}
+		end += 1;
+	}
+
+	foo.take(start, end);
+}
+
+fn footer<T>(foo: &mut Vec<T>, start_token: T, end_token: T)
+where T: PartialEq {
+	let mut end = foo.len() - 1;
+	loop {
+		if foo[end] == end_token {
+			break;
+		}
+		end -= 1;
+	}
+	let mut start = end - 1;
+	loop {
+		if foo[start] == start_token {
+			break;
+		}
+		start -= 1;
+	}
+	foo.take(start, end);
+}
+
+fn main() {
+	let mut foo: Vec<u8> = vec![0, 2, 1, 0, 3, 1];
+	let mut bar: Vec<u8> = vec![0, 2, 1, 0, 3, 1];
+	avaricious(foo.as_mut(), 0, 1);
+	println!("{:?}", foo);
+	header(foo.as_mut(), 0, 1);
+	println!("{:?}", foo);
+	footer(bar.as_mut(), 0, 1);
+	println!("{:?}", bar);
 }
